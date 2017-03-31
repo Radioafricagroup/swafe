b579ec3fc52ee828c9d7d0436ee4eaf8eed99dc4
ishuah91 <ishuah91@gmail.com>
2017-03-31 17:21:47 +0300

    added more features

--- swafe/cli.py
+++ swafe/cli.py
@@ -1,10 +1,13 @@
 import click
+import os
+import sys
 from lib import swf
 from botocore.exceptions import ClientError
 
 @click.group()
 @click.pass_context
 def run(context):
+    sys.path.append(os.getcwd())
     pass
 
 @run.command('list.domains')
@@ -35,7 +38,8 @@ def describe_domain(domain):
         return
 
     click.echo('- %s' % domain)
-    click.echo('\t description: %s' % response['domainInfo']['description'])
+    if response['domainInfo'].has_key('description'):
+        click.echo('\t description: %s' % response['domainInfo']['description'])
     click.echo('\t status: %s' % response['domainInfo']['status'])
     click.echo('\t retention perion: %s' % response['configuration']['workflowExecutionRetentionPeriodInDays'])
 
@@ -45,9 +49,11 @@ def describe_domain(domain):
 @click.option('--desc', help='Short description of the domain')
 def register_domain(domain, retention_period, desc):
     click.echo('Registering domain %s' % domain)
-
+    params = { 'name': domain, 'workflowExecutionRetentionPeriodInDays': retention_period }
+    if desc:
+        params['description'] = desc
     try:
-        response = swf.register_domain(name=domain, description=desc, workflowExecutionRetentionPeriodInDays=retention_period)
+        response = swf.register_domain(**params)
     except ClientError as e:
         print e.response['Error']['Code'], e.response['Error']['Message']
         return
@@ -66,3 +72,87 @@ def deprecate_domain(domain):
         return
 
     click.echo('Successfully deprecated %s ' % domain)
+
+@run.command('list.domain.workflows')
+@click.argument('domain', required=True)
+@click.option('--registered/--deprecated', default=True, help='Specify the registration status of the workflows to list, default is REGISTERED')
+@click.option('--name', help='If specified, lists the workflow type with this name')
+def list_domain_workflows(domain, registered, name):
+    status = 'REGISTERED' if registered else 'DEPRECATED'
+    click.echo('Fetching %s worflows in the domain %s ' % (status, domain))
+    params = dict(domain=domain, registrationStatus=status)
+    if name:
+        params['name'] = name
+    try:
+        response = swf.list_workflow_types(**params)
+    except ClientError as e:
+        print e.response['Error']['Code'], e.response['Error']['Message']
+        return
+    if len(response['typeInfos']) == 0:
+        if name:
+            click.echo('There are no %s workflows with the name %s in the domain %s' % (status, name, domain))
+            return
+        click.echo('There are no %s workflows in the domain %s ' % (status, domain))
+        return
+
+    click.echo('Domain: %s \n' % domain)
+
+    for workflow in response['typeInfos']:
+        click.echo(' %s - (version %s)' % (workflow['workflowType']['name'], workflow['workflowType']['version']))
+        click.echo('\t status: %s' % workflow['status'])
+        if workflow.has_key('description'):
+            click.echo('\t description: %s' % workflow['description'])
+        click.echo('\t created: %s \n' % workflow['creationDate'])
+
+@run.command('describe.workflow')
+@click.argument('workflowClassPath', required=True)
+def describe_workflow(workflowclasspath):
+
+    workflow = instantiate_class(workflowclasspath)
+    click.echo('Fetching workflow %s' % workflow.name)
+    try:
+        response = swf.describe_workflow_type(domain=workflow.domain,
+        workflowType=workflow.get_workflow_type())
+    except ClientError as e:
+        print e.response['Error']['Code'], e.response['Error']['Message']
+        return
+
+    click.echo(workflow.name)
+    click.echo('\t status: %s' % response['typeInfo']['status'])
+    click.echo('\t created:  %s' % response['typeInfo']['creationDate'])
+
+    if response['configuration'].has_key('description'):
+        click.echo('\t description: %s ' % response['configuration']['description'])
+
+@run.command('register.workflow')
+@click.argument('workflowClassPath', required=True)
+def register_workflow(workflowclasspath):
+
+    workflow = instantiate_class(workflowclasspath)
+    click.echo('Registering workflow %s' % workflow.name)
+    try:
+        response = swf.register_workflow_type(**workflow.get_params())
+    except ClientError as e:
+        print e.response['Error']['Code'], e.response['Error']['Message']
+        return
+    click.echo('Successfully regisered workflow %s in %s domain' % (workflow.name, workflow.domain))
+
+
+@run.command('deprecate.workflow')
+@click.argument('workflowClassPath', required=True)
+def deprecate_workflow(workflowclasspath):
+    workflow = instantiate_class(workflowclasspath)
+    click.echo('Deprecating workflow %s ' % workflow.name)
+    try:
+        response = swf.deprecate_workflow_type(domain=workflow.domain,
+        workflowType=workflow.get_workflow_type())
+    except ClientError as e:
+        print e.response['Error']['Code'], e.response['Error']['Message']
+        return
+    click.echo('Successfully deprecated %s ' % workflow.name)
+
+def instantiate_class(classpath):
+    modulename, classname = classpath.rsplit('.', 1)
+    module = __import__(modulename, fromlist=['*'])
+    cls = getattr(module, classname)
+    return cls()
--- swafe/workflow.py
+++ swafe/workflow.py
@@ -1,7 +1,14 @@
 class Workflow(object):
+    domain = None
     name = None
-    task_list = None
     version = None
+    taskList = None
+    description = None
+    taskStartToCloseTimeout = None
+    executionStartToCloseTimeout = None
+    taskPriority = None
+    childPolicy = None
+    lambdaRole = None
     tasks = []
 
     def __init__(self):
@@ -22,3 +29,25 @@ class Workflow(object):
 
     def next_task(self, prev_task='start'):
         return self.tasks[prev_task]
+
+    def get_workflow_type(self):
+        return { 'name': self.name, 'version': self.version }
+
+    def get_params(self):
+        params = dict(domain=self.domain, name=self.name, version=self.version)
+        if self.description:
+            params['description'] = self.description
+        if self.taskStartToCloseTimeout:
+            params['defaultTaskStartToCloseTimeout'] = self.taskStartToCloseTimeout
+        if self.executionStartToCloseTimeout:
+            params['defaultExecutionStartToCloseTimeout'] = self.executionStartToCloseTimeout
+        if self.taskList:
+            params['defaultTaskList'] = { 'name': self.taskList }
+        if self.taskPriority:
+            params['defaultTaskPriority'] = self.taskPriority
+        if self.childPolicy:
+            params['defaultChildPolicy'] = self.childPolicy
+        if self.lambdaRole:
+            params['defaultLambdaRole'] = self.lambdaRole
+
+        return params
